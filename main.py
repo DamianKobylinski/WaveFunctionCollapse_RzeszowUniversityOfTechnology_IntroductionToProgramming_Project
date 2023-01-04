16a01a038b062362b1eda925cc48ff74f8a556c5
DamianKobylinski <damiankob12@gmail.com>
2023-01-04 11:55:38 +0100

    changes to work properly

--- main.py
+++ main.py
@@ -11,18 +11,16 @@ class Preparations(object):
     """ Check every other cell around main cell """
 
     @classmethod
-    def set_every_element_around_direction(cls, coords, grid_size):
-        y, x = coords
-        width, height = grid_size
+    def set_every_element_around_direction(cls, coordinate, grid_size):
         dirs = []
 
-        if y > 0:
+        if coordinate[0] > 0:
             dirs.append((-1, 0))
-        if y < height - 1:
+        if coordinate[0] < grid_size[1] - 1:
             dirs.append((1, 0))
-        if x > 0:
+        if coordinate[1] > 0:
             dirs.append((0, -1))
-        if x < width - 1:
+        if coordinate[1] < grid_size[0] - 1:
             dirs.append((0, 1))
 
         return dirs
@@ -32,9 +30,9 @@ class Preparations(object):
     @classmethod
     def initialize_grid_with_coefficients(cls, grid_size, cell_values):
         grid_of_factor = []
-        for _ in range(grid_size[1]):
+        for x in range(grid_size[1]):
             row = []
-            for _ in range(grid_size[0]):
+            for y in range(grid_size[0]):
                 row.append(set(cell_values))
             grid_of_factor.append(row)
         return grid_of_factor
@@ -43,20 +41,21 @@ class Preparations(object):
     def check_which_cell_have_to_be_near(cls, view_grid):
         height = len(view_grid)
         width = len(view_grid[0])
+
         list_of_this_elements = set()
-        how_many_of_proper_elements_is_in_grid = {}
+        how_many_tiles_of_proper_elements_is_in_grid = {}
 
         for y, row in enumerate(view_grid):
             for x, cell in enumerate(row):
-                if cell not in how_many_of_proper_elements_is_in_grid:
-                    how_many_of_proper_elements_is_in_grid[cell] = 0
-                how_many_of_proper_elements_is_in_grid[cell] += 1
+                if cell not in how_many_tiles_of_proper_elements_is_in_grid:
+                    how_many_tiles_of_proper_elements_is_in_grid[cell] = 0
+                how_many_tiles_of_proper_elements_is_in_grid[cell] += 1
 
                 for next_cell_coordinate in Preparations.set_every_element_around_direction((y, x), (width, height)):
                     next_cell = view_grid[y + next_cell_coordinate[0]][x + next_cell_coordinate[1]]
                     list_of_this_elements.add((cell, next_cell, next_cell_coordinate))
 
-        return list_of_this_elements, how_many_of_proper_elements_is_in_grid
+        return list_of_this_elements, how_many_tiles_of_proper_elements_is_in_grid
 
     """
         Output - output generated grid 
@@ -67,11 +66,8 @@ class Preparations(object):
         for row in generated_grid:
             output_array = []
             for val in row:
-                color = colors_to_show[val]
-                output_array.append(color + val + colorama.Style.RESET_ALL)
-
+                output_array.append(colors_to_show[val] + val)
             print("".join(output_array))
-
 
 """
     WaveFunctionCollapse class - class containing wave function collapse algorithm
@@ -99,17 +95,16 @@ class WaveFunctionCollapse(object):
     """
 
     def get(self, coordinate):
-        y, x = coordinate
-        return self.grid_setup_for_working_on[y][x]
+        return self.grid_setup_for_working_on[coordinate[0]][coordinate[1]]
 
     """
         get_collapsed - get next iteration
     """
 
-    def get_collapsed(self, co_ords):
-        opts = self.get(co_ords)
-        assert (len(opts) == 1)
-        return next(iter(opts))
+    def get_collapsed(self, coordinate):
+        opts = self.get(coordinate)
+        if len(opts) == 1:
+            return next(iter(opts))
 
     """
         get_all_collapsed - get all row of iterations
@@ -120,8 +115,9 @@ class WaveFunctionCollapse(object):
         width = len(self.grid_setup_for_working_on[0])
 
         collapsed = []
+
         for y in range(height):
-            row: list[str] = []
+            row = []
             for x in range(width):
                 row.append(self.get_collapsed((y, x)))
             collapsed.append(row)
@@ -132,12 +128,10 @@ class WaveFunctionCollapse(object):
         shannon_entropy - shannon entropy algorithm of specific coordinate 
     """
 
-    def shannon_entropy(self, co_ords):
-        y, x = co_ords
-
+    def shannon_entropy(self, coordinate):
         sum_of_weights = 0
         sum_of_weight_log_weights = 0
-        for opt in self.grid_setup_for_working_on[y][x]:
+        for opt in self.grid_setup_for_working_on[coordinate[0]][coordinate[1]]:
             weight = self.weights_of_characters[opt]
             sum_of_weights += weight
             sum_of_weight_log_weights += weight * math.log(weight)
@@ -150,8 +144,8 @@ class WaveFunctionCollapse(object):
 
     def is_fully_collapsed(self):
         for row in self.grid_setup_for_working_on:
-            for sq in row:
-                if len(sq) > 1:
+            for how_many_cells_possibilities in row:
+                if len(how_many_cells_possibilities) > 1:
                     return False
         return True
 
@@ -159,31 +153,37 @@ class WaveFunctionCollapse(object):
         collapse - chose tile for getting coordinates
     """
 
-    def collapse(self, co_ords):
-        y, x = co_ords
-        opts = self.grid_setup_for_working_on[y][x]
-        filtered_tiles_with_weights = [(tile, weight) for tile, weight in self.weights_of_characters.items() if
-                                       tile in opts]
+    def collapse(self, coordinate):
+        opts = self.grid_setup_for_working_on[coordinate[0]][coordinate[1]]
+        filtered_tiles_with_weights = []
+        for tile, weight in self.weights_of_characters.items():
+            if tile in opts:
+                filtered_tiles_with_weights.append([tile,weight])
 
-        total_weights = sum([weight for _, weight in filtered_tiles_with_weights])
+        weights_on_specific_character = []
+        for _, weight in filtered_tiles_with_weights:
+            weights_on_specific_character.append(weight)
+
+        total_weights = sum(weights_on_specific_character)
+
         rnd = random.random() * total_weights
 
         chosen = filtered_tiles_with_weights[0][0]
+
         for tile, weight in filtered_tiles_with_weights:
             rnd -= weight
             if rnd < 0:
                 chosen = tile
                 break
 
-        self.grid_setup_for_working_on[y][x] = {chosen}
+        self.grid_setup_for_working_on[coordinate[0]][coordinate[1]] = {chosen}
 
     """
         constrain - remove tile which do not have to be in specific cell
     """
 
-    def constrain(self, co_ords, forbidden_tile):
-        y, x = co_ords
-        self.grid_setup_for_working_on[y][x].remove(forbidden_tile)
+    def constrain(self, coordinate, forbidden_tile):
+        self.grid_setup_for_working_on[coordinate[0]][coordinate[1]].remove(forbidden_tile)
 
 
 """
@@ -227,8 +227,9 @@ class ModelLearn(object):
     """
         propagate - check a nearby elements if entropy of those elements was changed
     """
-    def propagate(self, co_ords):
-        stack = [co_ords]
+
+    def propagate(self, coordinate):
+        stack = [coordinate]
 
         while len(stack) > 0:
             cur_co_ords = stack.pop()
@@ -237,9 +238,13 @@ class ModelLearn(object):
                 other_co_ords = (cur_co_ords[0] + d[0], cur_co_ords[1] + d[1])
 
                 for other_tile in set(self.wavefunction.get(other_co_ords)):
-                    other_tile_is_possible = any([
-                        self.check(cur_tile, other_tile, d) for cur_tile in cur_possible_tiles
-                    ])
+
+                    valid_option = []
+                    for tile in cur_possible_tiles:
+                        valid_option.append(self.check(tile, other_tile, d))
+
+                    other_tile_is_possible = any(valid_option)
+
                     if not other_tile_is_possible:
                         self.wavefunction.constrain(other_co_ords, other_tile)
                         stack.append(other_co_ords)
@@ -249,19 +254,17 @@ class ModelLearn(object):
     """
 
     def find_minimal_entropy_coordinates(self):
-        min_entropy = None
+        min_entropy = 0
         min_co_ords = (0, 0)
 
-        width, height = self.grid_size
-        for y in range(height):
-            for x in range(width):
+        for y in range(self.grid_size[0]):
+            for x in range(self.grid_size[1]):
                 if len(self.wavefunction.get((y, x))) == 1:
                     continue
 
-                entropy = self.wavefunction.shannon_entropy((y, x))
-                noise = entropy - (random.random() / 1000)
-                if min_entropy is None or noise < min_entropy:
-                    min_entropy = noise
+                entropy = self.wavefunction.shannon_entropy((y, x)) - random.random()
+                if entropy < min_entropy:
+                    min_entropy = entropy
                     min_co_ords = (y, x)
         return min_co_ords
 
